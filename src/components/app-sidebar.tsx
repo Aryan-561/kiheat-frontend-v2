b0e4d785e487b0baaff3e51c5eefb36665967d62
Aryan-561 <aryanmail561@gmail.com>
2025-12-07 18:31:49 +0530

    feat: add sidebar navigation

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,59 +1,114 @@
-import { BarChart2, Scale, BookOpen, BookText, Calendar, GraduationCap, Home, Inbox, Search, Settings, Trophy } from "lucide-react"
+"use client"
+
+import * as React from "react"
+import Link from "next/link"
+import {
+  Book,
+  Home,
+  Search,
+  GraduationCap,
+  User,
+  CalculatorIcon,
+  ScaleIcon,
+} from "lucide-react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavUser } from "@/components/nav-user"
 
 import {
   Sidebar,
   SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
   SidebarMenu,
-  SidebarMenuButton,
   SidebarMenuItem,
+  SidebarMenuButton,
+
 } from "@/components/ui/sidebar"
 
-const items = [
-  {
-    url: "#",
-    icon: Home,
+// This is sample data.
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
   },
-  {
-    url: "#",
-    icon: Search,
-  },
-  {
-    url: "#",
-    icon: BookOpen,
-  },
-  {
-    url: "#",
-    icon: Trophy,
-  },
-  {
-    url: "#",
-    icon: Scale,
-  },
-]
+  navMain: [
+    {
+      title: "Home",
+      url: "/",
+      icon: Home,
+    },
+    {
+      title: "Search",
+      url: "#",
+      icon: Search,
+    },
+    {
+      title: "Compare",
+      url: "#",
+      icon: ScaleIcon,
+    },
+    {
+      title: "Toppers",
+      url: "#",
+      icon: User,
+    },
+    {
+      title: "Estimate CGPA",
+      url: "#",
+      icon: CalculatorIcon,
+    },
+    {
+      title: "Programmes",
+      url: "#",
+      icon: Book,
+      isActive: true,
+      items: [
+        {
+          title: "BCA",
+          url: "#",
+        },
+        {
+          title: "BBA",
+          url: "#",
+        },
+        {
+          title: "B.COM",
+          url: "#",
+        },
+      ],
+    },
+  ],
+}
 
-export function AppSidebar() {
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar className=" bg-red-600">
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton>
+              <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                <GraduationCap className="size-4" />
+              </div>
+              <div className="grid flex-1 text-left text-sm leading-tight group-data-[collapsible=icon]:hidden">
+                <span className="truncate font-medium">KIHEAT Ranklist</span>
+
+              </div>
+
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupContent className="">
-            <SidebarMenu className="">
-              {items.map((item) => (
-                <SidebarMenuItem className="" key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
+        <NavMain items={data.navMain} />
       </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
     </Sidebar>
   )
 }
